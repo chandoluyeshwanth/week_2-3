8b881f8daaac9692face3e938fd1e138e7a59bfe
unknown <L00169749@student.lyit.ie>
2022-03-22 15:08:14 +0000

    adding

---
+++ manager_iss.R
@@ -0,0 +1,65 @@
+# This script builds the managers dataset
+# and populates it with data
+
+# Load data from previous session
+column_names <- c("Date", "Country", "Gender", "Age", "Q1", "Q2", "Q3", "Q4", "Q5")
+
+# Enter data into vectors before constructing the data frame
+date_col <- c("2018-15-10", "2018-01-11", "2018-21-10", "2018-28-10", "2018-01-05")
+country_col <- c("US", "US", "IRL", "IRL", "IRL")
+gender_col <- c("M", "F", "F", "M", "F")
+age_col <- c(32, 45, 25, 39, 99) # 99 is one of the values in the age attribute - will require recoding
+q1_col <- c(5, 3, 3, 3, 2)
+q2_col <- c(4, 5, 5, 3, 2)
+q3_col <- c(5, 2, 5, 4, 1)
+q4_col <- c(5, 5, 5, NA, 2) # NA is inserted in place of the missing data for this attribute
+q5_col <- c(5, 5, 2, NA, 1)
+
+# Construct a data frame using the data from all vectors above
+managers <- data.frame(date_col, country_col, gender_col, age_col, q1_col, q2_col, q3_col, q4_col, q5_col)
+
+# Add column names to data frame using column_names vector
+colnames(managers) <- column_names
+
+# Recode the incorrect 'age' data to NA
+managers$Age[managers$Age == 99] <- NA
+
+# Create a new attribute called AgeCat and set valuess
+# in AgeCat to the following if true:
+# <= 25 = Young
+# >= 26 & <= 44 = Middle Aged
+# >= 45 = Elderly
+# We will also recode age 'NA' to Elder
+
+managers$AgeCat[managers$Age >= 45] <- "Elder"
+managers$AgeCat[managers$Age >= 26 & managers$Age <= 44] <- "Middle Aged"
+managers$AgeCat[managers$Age <= 25] <- "Young"
+managers$AgeCat[is.na(managers$Age)] <- "Elder"
+
+
+summary(managers)
+
+
+# Recode AgeCat to make it 
+
+age_cat_ranked <- factor(managers$AgeCat,
+                         ordered = TRUE,
+                         levels = c("Young", "Middle Aged", "Elder"))
+managers$AgeCat <- age_cat_ranked
+str(managers)
+managers
+
+# Create a new column called summar_col
+# That contains 
+
+attach(managers)
+managers$summary_col <-c(Q1+ Q2 + Q3 + Q4 + Q5 )
+managers$mean_value <- rowMeans(managers[5:9],na.rm = FALSE)
+detach(managers)
+managers
+
+names(managers)[11] <- "Summary"
+names(managers)[12] <- "Mean"
+
+managers$Date <- as.Date(managers$Date, "%Y-%d-%m")
+str(managers)
